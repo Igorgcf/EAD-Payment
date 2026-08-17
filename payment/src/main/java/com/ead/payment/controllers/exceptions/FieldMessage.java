50d85267ea1090f2727452a5844809b61c6fa564
Igorgcf <igorTecnologia@outlook.com>
2026-08-17 00:09:32 -0300

    Field Message

---
+++ payment/src/main/java/com/ead/payment/controllers/exceptions/FieldMessage.java
@@ -0,0 +1,18 @@
+package com.ead.payment.controllers.exceptions;
+
+import lombok.Data;
+
+@Data
+public class FieldMessage {
+
+    private String fieldName;
+    private String fieldMessage;
+
+    public FieldMessage(){
+    }
+
+    public FieldMessage(String fieldName, String fieldMessage) {
+        this.fieldName = fieldName;
+        this.fieldMessage = fieldMessage;
+    }
+}
